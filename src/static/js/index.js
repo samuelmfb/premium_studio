c9581b1970e4edb8910c458567a3eb62f8ad968d
Samuel Marques <samuelmfb@gmail.com>
2023-03-25 08:03:18 -0300

    adicionada validação de login no load das paginas

--- src/static/js/index.js
+++ src/static/js/index.js
@@ -1,5 +1,9 @@
 const button = document.getElementById("bt-logout");
 
+$(document).ready(function() {
+    login_validation();
+})
+
 button.addEventListener("click", function(e){
     setCookie('premium_access', '', -1);
     setCookie('premium_refresh', '', -1);
@@ -27,4 +31,38 @@ for(let i = 0; i < ca.length; i++) {
     }
 }
 return "";
+}
+
+function login_validation() {
+    if (getCookie('premium_access') == "") {
+        alert('Usuário não logado. Redirecionando para a tela de login.');
+                window.location.replace("/login")
+    }
+    $.ajax({
+        url : "/api/v1/auth/me",
+        type : 'get',
+        contentType: "application/json; charset=utf-8",
+        async: false, 
+        headers: {"Authorization": "Bearer " + getCookie('premium_access')}
+   })
+    .done(function(response, msg, data){
+        console.log(response, msg);
+        
+   })
+   .fail(function(response, textStatus, msg){
+    console.log(response, msg);
+        if ('msg'in response['responseJSON']){ 
+            msg = response['responseJSON']['msg'];
+            if (msg == 'Token has expired') {
+                alert('Token expirou. Redirecionando para a tela de login.');
+                window.location.replace("/login");
+            };
+        } 
+        if ('error'in response['responseJSON']){
+            const error = response['responseJSON']['error'];
+            if (error) {
+                alert(error);
+            };
+        }
+    });
 }
---
+++ src/static/js/management.js
@@ -0,0 +1,37 @@
+$(document).ready(function() {
+    login_validation();
+})
+
+function login_validation() {
+    if (getCookie('premium_access') == "") {
+        alert('Usuário não logado. Redirecionando para a tela de login.');
+                window.location.replace("/login")
+    }
+    $.ajax({
+        url : "/api/v1/auth/me",
+        type : 'get',
+        contentType: "application/json; charset=utf-8",
+        async: false, 
+        headers: {"Authorization": "Bearer " + getCookie('premium_access')}
+   })
+    .done(function(response, msg, data){
+        console.log(response, msg);
+        
+   })
+   .fail(function(response, textStatus, msg){
+    console.log(response, msg);
+        if ('msg'in response['responseJSON']){ 
+            msg = response['responseJSON']['msg'];
+            if (msg == 'Token has expired') {
+                alert('Token expirou. Redirecionando para a tela de login.');
+                window.location.replace("/login");
+            };
+        } 
+        if ('error'in response['responseJSON']){
+            const error = response['responseJSON']['error'];
+            if (error) {
+                alert(error);
+            };
+        }
+    });
+}
