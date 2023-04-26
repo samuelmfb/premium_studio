3309d8471e87f8bc9080a5c497d4c66fad4424d4
Glauber Ferraz <102560669+glaubermferraz@users.noreply.github.com>
2023-04-25 21:55:37 -0300

    feat: mostrar aviso quando n existe projetos

--- src/static/js/projects.js
+++ src/static/js/projects.js
@@ -22,15 +22,18 @@ $(document).ready(function() {
         } else {
             projects.push(response)
         }
-        console.log(projects)
         for (project in projects) {
-            console.log(project)
+            if (project == 0){
+                html = "<h5>Sem projetos cadastrados para este cliente.</h5>"
+                break
+            }
             id = projects[project]['id_project'];
             html += "<div id='" + id +"' class='container-md bg-light-blue d-flex justify-content-between align-content-center ml-0 mb-3' onclick='show_project("+ id+")' > \
                 <p class='m-3'>" + projects[project]['name']+ "</p> \
             </div>"
         }
         list.innerHTML = html;   
+        
    })
    .fail(function(response, textStatus, msg){
         if ('msg'in response['responseJSON']){ 
